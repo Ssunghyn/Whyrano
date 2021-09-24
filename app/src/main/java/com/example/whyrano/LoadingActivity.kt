9d40bac8a742ae45e9ded4f4fb0c7744eaa3f54f
HanSungHyun <han990102@naver.com>
2021-09-25 00:10:59 +0900

    Loding Activity 구현
    로딩시간 2초 후 Login Activity로 이동

--- app/src/main/java/com/example/whyrano/LoadingActivity.kt
+++ app/src/main/java/com/example/whyrano/LoadingActivity.kt
@@ -1,11 +1,21 @@
 package com.example.whyrano
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import java.lang.Thread.sleep
+import kotlin.concurrent.thread
 
 class LoadingActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_loading)
+
+        thread {
+            sleep(2000L)
+            val intent = Intent(this,LoginActivity::class.java)
+            startActivity(intent)
+            finish()
+        }
     }
 }
