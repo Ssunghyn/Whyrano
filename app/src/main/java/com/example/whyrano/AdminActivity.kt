5c51487349737bbb9bb489facec469691630dd04
HanSungHyun <han990102@naver.com>
2021-09-26 01:22:35 +0900

    ToolBar 및 Navigation Drawer 구현
    User와 Admin 각 툴바 및 메뉴 창 구현

--- app/src/main/java/com/example/whyrano/AdminActivity.kt
+++ app/src/main/java/com/example/whyrano/AdminActivity.kt
@@ -2,10 +2,32 @@ package com.example.whyrano
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.MenuItem
+import androidx.core.view.GravityCompat
+import com.google.android.material.navigation.NavigationView
+import kotlinx.android.synthetic.main.activity_admin.*
 
 class AdminActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_admin)
+
+        setSupportActionBar(toolbar)
+        val actionBar = supportActionBar!!
+        actionBar.setTitle("")
+
+        navigationMenu.setOnClickListener {
+            navigation_layout.openDrawer(GravityCompat.START)
+        }
+
+    }
+
+    override fun onBackPressed() {
+        if (navigation_layout.isDrawerOpen(GravityCompat.START)){
+            navigation_layout.closeDrawers()
+        }
+        else {
+            super.onBackPressed()
+        }
     }
 }
--- app/src/main/java/com/example/whyrano/UserActivity.kt
+++ app/src/main/java/com/example/whyrano/UserActivity.kt
@@ -2,10 +2,31 @@ package com.example.whyrano
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.core.view.GravityCompat
+import kotlinx.android.synthetic.main.activity_admin.*
+import kotlinx.android.synthetic.main.activity_admin.toolbar
+import kotlinx.android.synthetic.main.activity_user.*
 
 class UserActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_user)
+        setSupportActionBar(toolbar)
+        val actionBar = supportActionBar!!
+        actionBar.setTitle("")
+
+        navigationMenuUser.setOnClickListener {
+            navigation_layout_user.openDrawer(GravityCompat.START)
+        }
+
+    }
+
+    override fun onBackPressed() {
+        if (navigation_layout_user.isDrawerOpen(GravityCompat.START)){
+            navigation_layout_user.closeDrawers()
+        }
+        else {
+            super.onBackPressed()
+        }
     }
 }
